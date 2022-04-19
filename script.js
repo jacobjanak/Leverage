298ad854a2dbaead14c104500e47bf9c333cf59b
Jacob Janak <jakeathotmail@gmail.com>
2022-04-19 15:06:49 -0400

    Users can buy LP correctly

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 $(document).ready(() => {
-	let i;
+	let i, u;
 	const users = [];
 
 	const contract = {
 		eth: 0,
+		ethup: 0,
+		ethdown: 0,
 		divider: 0,
 	};
 
 	$('button.buy-ethup').on('click', () => {
-		const amount = parseInt($('input.buy-ethup').val());
+		const amount = parseFloat($('input.buy-ethup').val());
+		if (u == null) return;
 		if (isNaN(amount)) return;
+		if (amount > users[u].eth) return;
 		$('input.buy-ethup').val('');
+		if (contract.ethup === 0) {
+			contract.ethup++;
+			users[u].ethup++;
+		} else {
+			const ratio = contract.ethup / (contract.eth - contract.divider);
+			contract.ethup += amount * ratio;
+			users[u].ethup += amount * ratio;
+		}
 		contract.eth += amount;
+		users[u].eth -= amount;
 		update();
 	})
 
@@ -21,9 +34,12 @@ $(document).ready(() => {
 
 	$('button.buy-ethdown').on('click', () => {
 		const amount = parseInt($('input.buy-ethdown').val());
+		if (u == null) return;
 		if (isNaN(amount)) return;
+		if (amount > users[u].eth) return;
 		$('input.buy-ethdown').val('');
 		contract.eth += amount;
+		users[u].eth -= amount;
 		contract.divider += amount;
 		update();
 	})
@@ -53,25 +69,47 @@ $(document).ready(() => {
 			$('.progress-bar').css({ width: 0 });
 		} else {
 			const midpoint = contract.divider / contract.eth;
-			$('#up-progress').css({ width: (1-midpoint) * 100 + '%' });
-			$('#down-progress').css({ width: midpoint * 100 + '%' });
+			$('#up-progress').css({ width: (1-midpoint)*100 + '%' });
+			$('#down-progress').css({ width: midpoint*100 + '%' });
 			$('#total-eth').text(contract.eth);
 		}
-	};
 
-	update();
+		// active user
+		$('.user-container').each((i, el) => {
+			if (i === u) {
+				$(el).children('.data-container').find('.eth-amount').text(users[i].eth);
+				$(el).children('.data-container').find('.ethup-amount').text(users[i].ethup);
+				$(el).children('.data-container').find('.ethdown-amount').text(users[i].ethdown);
+			}
+		})
+	}; update();
+
+	const handleIconClick = function() {
+		const index = parseInt($(this).attr('data-index'));
+		if (u != index) {
+			u = index;
+			$('.active').hide();
+			$('.inactive').show();
+			$(this).children('.inactive').hide();
+			$(this).children('.active').show();
+		}
+	}
 
 	const addUsers = () => {
 		if (users.length > 0) $('#users').show();
 		else $('#users').hide();
 		for (i = 0; i < users.length; i++) {
 			const $div = $('#user-template').clone();
-			$div.removeAttr('id');
+			$div.removeAttr('id').addClass('user-container');
+			$div.children('.icon-container').attr('data-index', i);
+			$div.children('.icon-container').on('click', handleIconClick);
+			$div.children('.icon-container').children('.active').hide();
 			$div.children('.name-container').children('.name').text(users[i].name);
+			$div.children('.data-container').find('.eth-amount').text(users[i].eth);
+			$div.children('.data-container').find('.ethup-amount').text(users[i].ethup);
+			$div.children('.data-container').find('.ethdown-amount').text(users[i].ethdown);
 			$('#users').append($div);
 		}
-	};
-
-	addUsers();
+	}; addUsers();
 
 })
